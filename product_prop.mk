16d35ad766a4d665e096441b6784ab62b0ddf43d
Slawek <yarpiin@gmail.com>
2020-05-13 18:40:12 +0100

    universal9810: update ringtones

--- product_prop.mk
+++ product_prop.mk
@@ -31,6 +31,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Tones
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.ringtone=Xylophone_Dubstep.ogg \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Hassium.ogg \
+    ro.config.notification_sound=Twinkle.ogg \
+    ro.config.alarm_alert=Cuckoo_clock.ogg \
     ro.config.vc_call_vol_steps=7
